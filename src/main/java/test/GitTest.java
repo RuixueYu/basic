b1c6819f7a3c5e132b11c4ada9fbc9ce82a788cd
yuruixue <1320448244@qq.com>
2022-08-15 09:50:30 +0800

    新建测试文件

---
+++ src/main/java/test/GitTest.java
@@ -0,0 +1,12 @@
+package test;
+
+/**
+ * @Author: YuRuixue
+ * @Description: TODO
+ * @date 2022/8/15/0015 9:48
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("I have learned git!");
+    }
+}
